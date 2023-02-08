851d38321109d52af8e9862e3e5b614fb927d341
kyle_karotko <kkrtk7@duck.com>
2023-02-08 10:45:37 -0500

    adding routes to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,8 +12,42 @@ router.get('/',async (req, res) => {
     });
 })
 
+router.get('/post/:id',async (req, res) => {
+  const postData = await Post.findByPk(req.params.id, {include:[User]})
+  const posts = post.get({ plain: true });
 
+  res.render('post', { 
+    posts, 
+    logged_in: req.session.logged_in 
+  });
+})
 
+router.get('/viewpost/:id',async (req, res) => {
+  const postData = await Post.findByPk(req.params.id, {include:[User]})
+  const posts = post.get({ plain: true });
 
+  res.render('viewpost', { 
+    posts, 
+    logged_in: req.session.logged_in 
+  });
+})
+
+router.get('/dashboard',withAuth, async (req, res) => {
+  const postData = await Post.findAll({where:{userId: req.session.userId}})
+  const posts = postData.map((post) => post.get({ plain: true }));
+
+  res.render('dashboard', { 
+    posts, 
+    logged_in: req.session.logged_in 
+  });
+})
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/dashboard')
+    return;
+  }
+  res.render('login');
+})
 
 module.exports=router;
