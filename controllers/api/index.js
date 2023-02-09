203e3dfd74e18c23d466cbfd96b3242db8279730
kyle_karotko <kkrtk7@duck.com>
2023-02-08 22:50:22 -0500

    fixing typo on import after accidently deleting file and renaming

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const userRoutes = require("./userRoutes");
 const postRoutes = require("./postRoutes");
-const commentRoutes = require("./commentRoutes");
+const commentsRoutes = require("./commentsRoutes");
 
 router.use("/user", userRoutes);
 router.use("/post", postRoutes);
-router.use("/comment", commentRoutes);
+router.use("/comment", commentsRoutes);
 
 module.exports=router;
